7e012ccd1c32012a3d5a9b1d856517b2b8bea0c4
tatsupiko <identity2022@outlook.jp>
2023-05-19 19:19:02 +0900

    [Add] BoidAlgorithmクラスがアタッチされているオブジェクトが参照するパラメーターが格納されたスクリプタブルオブジェクト生成するクラスを作成した。

---
+++ Assets/Parameter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boid Param")]
+
+// 日本語対応
+public class Parameter : ScriptableObject
+{
+    public float initSpeed = 2f;
+    public float minSpeed = 2f;
+    public float maxSpeed = 5f;
+    public float neighborDistance = 1f;
+    public float neighborFov = 90f;
+    public float separationWeight = 5f;
+    //public float wallScale = 5f;
+    //public float wallDistance = 3f;
+    //public float wallWeight = 1f;
+    public float alignmentWeight = 2f;
+    public float cohesionWeight = 3f;
+}
